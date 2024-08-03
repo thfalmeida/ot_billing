45939b9a7646dc8c8207a9b69aa8ae97d0044eba
thfalmeida <thiago.farias.almeida@ccc.ufcg.edu.br>
2024-08-03 17:22:32 -0300

    HotFix viagem

--- src/main/java/com/oteasy/ot_billing/model/Viagem.java
+++ src/main/java/com/oteasy/ot_billing/model/Viagem.java
@@ -1,5 +1,8 @@
 package com.oteasy.ot_billing.model;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
@@ -15,7 +18,8 @@ import lombok.Setter;
 @NoArgsConstructor
 public class Viagem implements ObjectModel {
     private int id;
-    private long data;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date dataRegistro;
     private long km_inicial;
     private long km_final;
     private int motorista_id;
--- src/main/java/com/oteasy/ot_billing/model/ViagemInfo.java
+++ src/main/java/com/oteasy/ot_billing/model/ViagemInfo.java
@@ -1,5 +1,8 @@
 package com.oteasy.ot_billing.model;
 
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
@@ -11,16 +14,16 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ViagemInfo implements ObjectModel {
-    public int id;
-    public long data;
-    public long km_inicial;
-    public long km_final;
-    public String motorista;
-    public String carro;
-    public String cliente;
-    public int qnt_transporte;
-    public String observacao;
-    public int num_ajudante;
-
-    
+    private int id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy")
+    private Date dataRegistro;
+    private long km_inicial;
+    private long km_final;
+    private String motorista;
+    private String carro;
+    private String cliente;
+    private float valor;
+    private int qnt_transporte;
+    private String observacao;
+    private int num_ajudante;
 }
